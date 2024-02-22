03370b82ea332bf535b77a3bff30169a9a3971f0
Giga <asefhosseini00@gmail.com>
2024-02-23 00:57:26 +0330

    create header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="flex justify-between">
+    <img src="@/assets/logo.png" alt="logo" />
+
+    <nav class="flex gap-1">
+      <span class="icon-search"></span>
+      <RouterLink to="/">خانه</RouterLink>
+      <RouterLink :to="{ name: 'contact-us' }">ارتباط با ما</RouterLink>
+      <RouterLink :to="{ name: 'about-us' }">درباره ما</RouterLink>
+    </nav>
+
+    <div class="grid gap-2">
+      <button class="rounded-lg bg-primary-500">ثبت نام</button>
+      <button class="rounded-lg text-primary-500">ورود</button>
+    </div>
+  </header>
+</template>
